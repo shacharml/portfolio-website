b5236b4f3f8da15bfed18f52c81594fd143bd2ac
shacharml <shacharml611@gmail.com>
2025-02-12 10:15:52 +0200

    Create navbar

---
+++ protfolio-website/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main className="mx-auto max-w-7xl overflow-x-hidden antialiased">
+      <div className="bg-image fixed inset-0 bg-cover bg-fixed bg-center"></div>
+      <div className="relative z-10">
+        <Navbar />
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ protfolio-website/src/components/Navbar.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { NAVIGATION_LINKS } from "../../constants";
+import { RiCloseLine, RiMenu3Line } from "@remixicon/react";
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleLinkClick = (e, href) => {
+    e.preventDefault();
+    const targetElement = document.querySelector(href);
+    if (targetElement) {
+      const offset = -85;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.scrollY + offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+    setIsMobileMenuOpen(false);
+  };
+
+  return (
+    <div>
+      <nav className="fixed left-0 right-0 z-50 lg:top-4">
+        {/* {desktop Menu} */}
+        <div
+          className="mx-auto hidden max-w-2xl items-center justify-between rounded-full border border-white/30 
+            px-6 py-2 backdrop-blur-lg lg:flex"
+        >
+          <div className="flex items-center justify-between gap-4">
+            <div>
+              <a href="/">
+                <span className="uppercase">Shachar Males </span>
+              </a>
+            </div>
+          </div>
+
+          <ul className="flex items-center gap-4">
+            {NAVIGATION_LINKS.map((item, index) => (
+              <li key={index}>
+                <a
+                  className="text-sm hover:text-stone-300"
+                  href={item.href}
+                  onClick={(e) => handleLinkClick(e, item.href)}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* {Mobile Menu} */}
+        <div className="py-2 backdrop-blur-md lg:hidden">
+          <div className="flex items-center justify-between">
+            <div>
+              <a href="#">
+                <span className="pl-2 uppercase">Shachar Males</span>
+              </a>
+            </div>
+            <div className="flex items-center">
+              <button
+                className="focus:outline-none lg:hidden"
+                onClick={toggleMobileMenu}
+                aria-label={isMobileMenuOpen ? "Close Menu" : "Open Menu"}
+              >
+                {isMobileMenuOpen ? (
+                  <RiCloseLine className="m-2 h-6 w-5" />
+                ) : (
+                  <RiMenu3Line className="m-2 h-6 w-5" />
+                )}
+              </button>
+            </div>
+          </div>
+          {isMobileMenuOpen && (
+            <ul className="m-4 ml-4 flex flex-col gap-6 backdrop-blur-md">
+              {NAVIGATION_LINKS.map((item, index) => (
+                <li key={index}>
+                  <a
+                    href={item.href}
+                    className="block w-full next-lg"
+                    onClick={(e) => handleLinkClick(e, item.href)}
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
