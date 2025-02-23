2f84ff745ccfd330485a0e7ee799224d1877c058
shacharml <shacharml611@gmail.com>
2025-02-23 14:12:47 +0200

    add react icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,8 +12,7 @@ const Hero = () => {
     >
       <div className="mt-8 lg:mt-20">
         <h1 className="hero-titel text-4xl uppercase lg:text-7xl mt-20">
-          
-          {"Update" /* {PROFILE.name} */}
+          {PROFILE.name}
         </h1>
         <h2
           className="hero-subtitle bg-gradient-to-b from-pink-200 to-purple-300 bg-clip-text text-center text-2xl -tracking-tighter
