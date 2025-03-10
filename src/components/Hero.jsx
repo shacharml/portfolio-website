10bd6d1fd05235b13cd2f17910caa9ee282a9736
shacharml <shacharml611@gmail.com>
2025-03-10 15:11:08 +0200

    change color back

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,11 +12,11 @@ const Hero = () => {
       rel={heroRef}
     >
       <div className="mt-8 lg:mt-20">
-        <h1 className="hero-titel text-4xl uppercase lg:text-7xl mt-20 outline-orange-400 outline-dotted">
+      <h1 className="hero-titel text-4xl uppercase lg:text-7xl mt-20">
           {PROFILE.name}
         </h1>
         <h2
-          className="hero-subtitle bg-gradient-to-b from-red-700 to-lime-600 bg-clip-text text-center text-2xl -tracking-tighter
+          className="hero-subtitle bg-gradient-to-b from-pink-200 to-purple-300 bg-clip-text text-center text-2xl -tracking-tighter
        text-transparent "
         >
           {PROFILE.role}
